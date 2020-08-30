5b9043fac1739dda4360635edcd29dd54fef2609
qunahui <quanhui812@gmail.com>
2020-08-30 15:23:38 +0700

    Fixing scrollbar

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -6,7 +6,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   if (existingCartItem) {
     return cartItems.map((item) =>
       item.id === cartItemToAdd.id
-        ? { ...cartItems, quantity: item.quantity + 1 }
+        ? { ...item, quantity: item.quantity + 1 }
         : item
     );
   }
