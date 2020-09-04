cc30b0c921b63b6afc7fe7d73a7b2a27059070be
qunahui <quanhui812@gmail.com>
2020-09-04 16:19:29 +0700

    Fixing build error

--- client/src/components/with-spinner/with-spinner.component.jsx
+++ client/src/components/with-spinner/with-spinner.component.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { SpinnerOverlay, SpinnerContainer } from './with-spinner.styles';
 
-const WithSpinner = (WrapperComponent) => ({
-  isLoading,
-  ...otherProps,
-}) => {
-  return isLoading ? <SpinnerOverlay><SpinnerContainer/></SpinnerOverlay> : <WrapperComponent {...otherProps}/>
+const WithSpinner = (WrapperComponent) => ({ isLoading, ...otherProps }) => {
+  return isLoading ? (
+    <SpinnerOverlay>
+      <SpinnerContainer />
+    </SpinnerOverlay>
+  ) : (
+    <WrapperComponent {...otherProps} />
+  );
 };
 
 export default WithSpinner;
